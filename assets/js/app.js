013c576f1b2ac82592aa91b1828404a93a37d0fd
Faruqoloyede <oloyedefaruq2@gmail.com>
2023-08-02 13:21:25 -0700

    desktop view

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".slider", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
